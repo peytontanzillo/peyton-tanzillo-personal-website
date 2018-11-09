4d778d690e402a188447a06126827177a0ef76fd
Peyton Tanzillo <ptown16@gmail.com>
2018-11-09 15:20:23 -0600

    Added a single looping raindrop

--- script.js
+++ script.js
@@ -6,19 +6,36 @@ class Raindrop {
       width: width, 
       height: height
     }, 0);
+    $('.drop').eq(this.id).stop();
     this.animationSpeed = animationSpeed;
   }
   
-  animateDown() {
+  resetDrop() {
+    console.log('reset');
     $('.drop').eq(this.id).animate({
-      y: '1000'
+      y: '0'
+    }, 0);
+  }
+  
+  animate() {
+    console.log('animate');
+    $('.drop').eq(this.id).animate({
+      y: '2000'
     }, this.animationSpeed);
   }
+  
 };
 
-console.log('here');
-
 $( document ).ready(function() {
-  r = new Raindrop(0, 7, 30, 500, 2000);
-  r.animateDown();
+  for (let i = 0; i < $('.drop').length; i++) {
+    drop = new Raindrop(i, 7, 30, 500, 4000);
+  }
+  
+  r = new Raindrop(0, 7, 30, 500, 4000);
+  
+  setInterval(function() {
+    r.animate();
+    r.resetDrop();
+  }, r.animationSpeed);
+  
 });
