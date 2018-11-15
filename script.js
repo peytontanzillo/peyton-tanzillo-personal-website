4fab91fcd9483d2075750f4ebccf7f522971309b
Peyton Tanzillo <ptown16@gmail.com>
2018-11-15 15:57:50 -0600

    Cleaned up javascript using ESLint

--- script.js
+++ script.js
@@ -1,83 +1,95 @@
+// Constants for Raindrop
+const RAIN_START_TOP = -100;
+const RAIN_END_TOP = 2000;
+const NUMBER_OF_RAINDROPS = 50;
+
+// Bounds for Raindrop
+const WIDTH_LOWER_BOUND = 6;
+const WIDTH_UPPER_BOUND = 12;
+const HEIGHT_LOWER_BOUND = 40;
+const HEIGHT_UPPER_BOUND = 70;
+const ANIMATION_SPEED_LOWER_BOUND = 2000;
+const ANIMATION_SPEED_UPPER_BOUND = 4000;
+
+const DOWN_ARROW_INTERVAL = 1000;
+
+const SECTION_SCROLL_SPEED = 800;
+
 class Raindrop {
   constructor(drop, width, height, x, animationSpeed) {
     this.drop = drop;
-    this.drop.css({position: 'absolute', width: width, height: height});
-    this.drop.offset({top: -100, left: x});
+    this.drop.css({
+      position: 'absolute',
+      width,
+      height,
+    });
+    this.drop.offset({
+      top: RAIN_START_TOP,
+      left: x,
+    });
     this.animationSpeed = animationSpeed;
   }
-  
-  resetDrop() {
-    this.drop.animate({
-      top: '-100'
-    }, 0);
-  }
-  
-  animate() {
-    this.drop.animate({
-      top: '2000'
-    }, this.animationSpeed);
-  }
-  
-};
 
-$( document ).ready(function() {
-  
+  resetDrop() {
+    this.drop.animate({top: RAIN_START_TOP}, 0);
+  }
+
+  animate() {
+    this.drop.animate({top: RAIN_END_TOP}, this.animationSpeed);
+  }
+}
+
+function createRainDivs() {
+  for (let i = 0; i < NUMBER_OF_RAINDROPS; i++) {
+    $('#raindrops').append('<div class="drop"></div>');
+  }
+}
+
+function makeRain() {
+  const allDrops = [];
+
+  for (let i = 0; i < $('.drop').length; i++) {
+    const width = ((WIDTH_UPPER_BOUND - WIDTH_LOWER_BOUND) * Math.random()) + WIDTH_LOWER_BOUND;
+    const height = ((HEIGHT_UPPER_BOUND - HEIGHT_LOWER_BOUND) * Math.random()) + HEIGHT_LOWER_BOUND;
+    const position = ($(window).width() + (2 * width)) * Math.random();
+    const animationSpeed = ((ANIMATION_SPEED_UPPER_BOUND - ANIMATION_SPEED_LOWER_BOUND) * Math.random()) + ANIMATION_SPEED_LOWER_BOUND;
+    const drop = new Raindrop($('.drop').eq(i), width, height, position, animationSpeed);
+    allDrops.push(drop);
+    setInterval(() => {
+      allDrops[i].animate();
+      allDrops[i].resetDrop();
+    }, allDrops[i].animationSpeed);
+  }
+}
+
+$(document).ready(() => {
   createRainDivs();
-  
+
   makeRain();
-  
-  setInterval(function() {
-    $('#down-arrow').animate({
-      margin: '6px 0px'
-    }, 1000);
-    setTimeout(function() {
-      $('#down-arrow').animate({
-        margin: '-6px 0px'
-      }, 1000);
-    }, 1000);
-  }, 2000); 
-  
-  $('.scroll-to-id').click(function() {
-    const correctID = this.id.split("-");
-    $('html, body').animate({
-      scrollTop: $(correctID[1]).position().top + $(window).height()
-    }, 800);
+
+  setInterval(() => {
+    $('#down-arrow').animate({margin: '6px 0px'}, DOWN_ARROW_INTERVAL);
+    setTimeout(() => {
+      $('#down-arrow').animate({margin: '-6px 0px'}, DOWN_ARROW_INTERVAL);
+    }, DOWN_ARROW_INTERVAL);
+  }, 2 * DOWN_ARROW_INTERVAL);
+
+  $('.scroll-to-id').click(() => {
+    const correctID = this.id.split('-');
+    $('html, body').animate({scrollTop: $(correctID[1]).position().top + $(window).height()}, SECTION_SCROLL_SPEED);
   });
-  
-  $(window).resize(function() {
+
+  $(window).resize(() => {
     makeRain();
   });
-  
+
   $('.skill-full').hide();
   let hasAnimatedSkills = false;
-  
-  $(document).scroll(function() {
+
+  $(document).scroll(() => {
     if ($(document).scrollTop() >= ($('#skills').position().top + $(window).height()) && !hasAnimatedSkills) {
       hasAnimatedSkills = true;
       $('.skill-full').toggle('slide');
     }
   });
 });
-
-function createRainDivs() {
-  for (i = 0; i < 50; i++) {
-    $('#raindrops').append("<div class=\"drop\"></div>");
-  }
-}
-
-function makeRain() {
-  allDrops = [];
-  
-  for (let i = 0; i < $('.drop').length; i++) {
-    const width = (6 * Math.random()) + 6;
-    const height = (30 * Math.random()) + 40;
-    const position = (($(window).width() + (2 * width)) * Math.random());
-    const animationSpeed = (2000 * Math.random()) + 2000;
-    const drop = new Raindrop($('.drop').eq(i), width, height, position, animationSpeed);
-    allDrops.push(drop);
-    setInterval(function() {
-      allDrops[i].animate();
-      allDrops[i].resetDrop();
-    }, allDrops[i].animationSpeed);
-  }
-}
